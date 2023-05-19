d6f06fab55145d6518c495c9a3f76cb2b6c0aa6e
Janne Mareike Koschinski <mail@justjanne.de>
2023-05-19 17:24:52 +0200

    Use a separate template for the displayname (#11)
    * Update module api
    * Use a separate displayname template instead of the localpart
    * Add tests for template handling
    * Lockfile maintenance

--- src/components/AskNameDialog.tsx
+++ src/components/AskNameDialog.tsx
@@ -20,8 +20,9 @@ import { DialogContent } from "@matrix-org/react-sdk-module-api/lib/components/D
 import { TextInputField } from "@matrix-org/react-sdk-module-api/lib/components/TextInputField";
 import { Spinner } from "@matrix-org/react-sdk-module-api/lib/components/Spinner";
 import { AccountAuthInfo } from "@matrix-org/react-sdk-module-api/lib/types/AccountAuthInfo";
+import { ModuleApi } from "@matrix-org/react-sdk-module-api/lib/ModuleApi";
 
-import { fillLocalpart, fillPassword, TemplateVars } from "../templates";
+import { fillDisplayname, fillLocalpart, fillPassword, TemplateVars } from "../templates";
 
 interface Props extends DialogProps {
     // we don't need anything new
@@ -36,16 +37,23 @@ export interface AccountModel {
     creds: AccountAuthInfo;
 }
 
+export async function registerAccount(moduleApi: ModuleApi, vars: TemplateVars): Promise<AccountAuthInfo> {
+    const localpart = fillLocalpart(moduleApi, vars);
+    const password = fillPassword(moduleApi, vars);
+    const displayName = fillDisplayname(moduleApi, vars);
+    return moduleApi.registerSimpleAccount(
+        localpart, password, displayName,
+    );
+}
+
 export class AskNameDialog extends DialogContent<Props, State, AccountModel> {
     public async trySubmit(): Promise<AccountModel> {
         this.setState({ busy: true });
-        const vars: TemplateVars = {
+        const creds = await registerAccount(this.props.moduleApi, {
             firstName: this.state.firstName,
             lastName: this.state.lastName,
-        };
-        const localpart = fillLocalpart(this.props.moduleApi, vars);
-        const password = fillPassword(this.props.moduleApi, vars);
-        const creds = await this.props.moduleApi.registerSimpleAccount(localpart, password, localpart);
+        });
+        this.setState({ busy: false });
         return { creds };
     }
 
